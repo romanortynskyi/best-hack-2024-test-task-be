8e3394b1172c55f5ea5b498e079689d5dd56d4b3
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2024-04-29 12:42:06 +0300

    Fix

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,7 @@
-SECRET_NAME="best-hack-2024-test-task-be/sqlalchemy-database-uri"
+SECRET_ARN="arn:aws:secretsmanager:us-east-1:891377136147:secret:best-hack-2024-test-task-be/sqlalchemy-database-uri-mdOYL2"
 
 # Retrieve the secret value
-SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ARN --query SecretString --output text)
 
 # Export the secret value as an environment variable
 export SQKALCHEMY_DATABASE_URI=$SECRET_VALUE
